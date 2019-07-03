7196e0e7b1260b1eba86822e91172a3ac2b76e31
Colby Allen <colbyallen012@gmail.com>
2019-07-02 18:41:21 -0600

    Display people cards, minor css
    Co-authored-by: Jacqueline Moore <jacquelinebmoore@gmail.com>

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React from 'react'
+import Card from './Card'
 import './CardContainer.css'
 
-class CardContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      activeButton: ''
-    }
-  }
+const CardContainer = ({content}) => {
 
-  findContent = e => {
-    this.setState({ activeButton: e.target.textContent })
-  }
-
-  render() {
+  const peopleCards = content.map(person => {
     return (
-      <main>
-        <button 
-          onClick={event => this.findContent(event)}
-        >People</button>
-        <button
-          onClick={event => this.findContent(event)}
-        >Planets</button>
-        <button
-          onClick={event => this.findContent(event)}
-        >Vehicles</button>
-        <button
-          onClick={event => this.findContent(event)}
-        >Favorites</button>
-        { this.state.activeButton === 'people' && <Card content={ this.props.people } />}
-      </main>
+      <Card
+        name={person.name}
+        birthYear={person.birth_year}
+        gender={person.gender}
+        height={person.height}
+        eyeColor={person.eye_color}
+      />
     )
-  }
+  })
+
+  // const planetCards = content.map(planet => {
+  //   return (
+  //     <Card
+  //       name={planet.name}
+  //     />
+  //   )
+  // })
+
+
+  return (
+    <div className='card-area'>
+      {peopleCards}
+      {/* {planetCards} */}
+    </div>
+  )
 }
 
 export default CardContainer;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import './NavBar.css';
+import Card from './Card';
+import './NavBar.css'
+import CardContainer from './CardContainer';
 
-const NavBar = props => {
-  return (
-    <section>
-      <button >People</button>
-      <button>Planets</button>
-      <button>Vehicles</button>
-      <button>Favorites</button>
-    </section>
-  )
+class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeButton: ''
+    }
+  }
+
+  findContent = e => {
+    this.setState({ activeButton: e.target.textContent })
+  }
+
+  render() {
+    return (
+      <main>
+        <button 
+          onClick={event => this.findContent(event)}
+        >People</button>
+        <button
+          onClick={event => this.findContent(event)}
+        >Planets</button>
+        <button
+          onClick={event => this.findContent(event)}
+        >Vehicles</button>
+        <button
+          onClick={event => this.findContent(event)}
+        >Favorites</button>
+      <section>
+        {this.state.activeButton === 'People' && <CardContainer content={this.props.people} />}
+        {/* {this.state.activeButton === 'Planets' && <CardContainer content={this.props.planets} />} */}
+      </section>
+      </main>
+    )
+  }
 }
 
 export default NavBar;

416b76dc53502389669d6fb3a5c1d4649c0edb14
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-07-09 16:46:12 -0600

    Get test to pass for addFavorite
    Co-authored-by: Colby <colbyallen012@gmail.com>

--- src/Components/Navbar/NavBar.test.js
+++ src/Components/Navbar/NavBar.test.js
@@ -3,13 +3,54 @@ import { shallow, mount } from 'enzyme';
 import NavBar from './NavBar';
 
 describe('NavBar', () => {
-  let wrapper;
+  let navBarComponent;
+  let mockPeople;
+  let mockVehicles;
+  let mockPlanets;
+  let mockFavorites;
+  let mockCard;
 
   beforeEach(() => {
-    wrapper = shallow(<NavBar />)
+    mockPeople = [
+      {name: 'Tom', gender: 'female'},
+      {name: 'Jenny', gender: 'female'}
+    ]
+    mockVehicles = [
+      {name: 'boat', length: '2'},
+      {name: 'car', length: '900'}
+    ]
+    mockPlanets = [
+      {name: 'boops', climate: 'hot'},
+      {name: 'fanta', climate: 'no'}
+    ]
+
+    mockFavorites = [
+      {name: 'Jenny', gender: 'female'},
+      {name: 'fanta', climate: 'no'}
+    ]
+
+    mockCard = {name: 'batman', climate: 'windy'};
+
+    navBarComponent = shallow(
+      <NavBar 
+        people={mockPeople}
+        vehicles={mockVehicles}
+        planets={mockPlanets} 
+      />
+    )
+    
+    navBarComponent.setState({ favorites: mockFavorites });
+
   });
 
   it('should match snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(navBarComponent).toMatchSnapshot();
   });
+
+  it('should update favorites array in state when addFavorite is called', () => {
+    const expected = [...mockFavorites, mockCard];
+    navBarComponent.instance().addFavorite(mockCard)
+
+    expect(navBarComponent.state('favorites')).toEqual(expected);
+  })
 });

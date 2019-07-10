7dbc9024d28c6f7dd4046d922dd1987f115eefde
Colby Allen <colbyallen012@gmail.com>
2019-07-09 20:35:13 -0600

    clean up css

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Landing.css'
 
 const Landing = () => {
   return(
     <>
-      <h2>May the Force be With You</h2>
-      <h3>Please pick a category</h3>
+      <h2 className='title'>May the Force be With You</h2>
+      <h3 className='pick'>Please pick a category</h3>
     </>
   )
 }
